b558a3d6e109a3f4c9163dad56da5e5bf8b0361a
Alfredo-1 <alfr3d0zurita@gmail.com>
2026-04-28 23:02:56 -0300

    funcion para cargar nuevas tareas(completa), crear nodo e insertar nodo (completas)

--- ModuloToDo.c
+++ ModuloToDo.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct Tarea{ 
     int TareaID;//Numérico autoincremental comenzando en 1000 
     char *Descripcion;  //       
     int Duracion; // entre 10 – 100  
-    } Tarea; 
+} Tarea; 
 
 typedef struct Nodo{ 
     Tarea T;  
@@ -15,30 +16,25 @@ typedef struct Nodo{
 
 void limpiarBuffer();
 Nodo * crearListaVacia();
-Nodo * crearNodo(int id, int duracion, char Descripcion);
-void ingresarTarea();
+Nodo * crearNodo(Tarea nuevaTarea);
+Tarea cargarTarea(int id);
+void insertarNodo(Nodo ** Start, Nodo * nuevoNodo);
 
 int main()
 {
+    srand(time(NULL));
     Nodo * start = crearListaVacia();
-    Tarea nuevaTarea;
-    char buffer[100];
-    int opcion = 0;
+    Tarea tareaNueva;
+    
     int condicion = 0;
-    int id = 1000;
-   do
-   {
-        printf("\n---------Cargar tarea--------");
-        printf("\n\ningrese 'descripcion' de tarea pendiente: ");
-        fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")]= '\0';
-        nuevaTarea.Descripcion = (char *) malloc((strlen(buffer)+1) * sizeof(char));
-        strcpy(nuevaTarea.Descripcion, buffer);
+    int opcion = 0;
+    static int id = 999;
 
-        nuevaTarea.Duracion = 10 + rand() % 91;
-
+    do
+    {
         id++;
-        nuevaTarea.TareaID = id;
+        tareaNueva = cargarTarea(id);
+        insertarNodo(&start, crearNodo(tareaNueva));
 
         printf("\n-----Seleccione una opcion-----------\n\n");
         printf("1. Ingresar nueva tarea\n2. Finalizar carga de tarea");
@@ -55,14 +51,18 @@ int main()
             condicion = 0;
 
     } while (condicion == 1);
+    Nodo * Aux = start;
+    printf("\n\n---------------Las tareas son------------");
+    while (Aux)
+    {
+        printf("\n\ndescripcion de tarea: %s", Aux->T.Descripcion);
+        printf("\nduracion de tarea: %d horas", Aux->T.Duracion);
+        printf("\nID Tarea: %d", Aux->T.TareaID);
+
+        Aux = Aux->Siguiente;
+    }
     
     
-
-    printf("\n\nnueva tarea es: %s", nuevaTarea.Descripcion);
-    printf("\nduracion de tarea: %d horas", nuevaTarea.Duracion);
-    printf("\nID Tarea: %d", nuevaTarea.TareaID);
-
-    free(nuevaTarea.Descripcion);
     return 0;
 }
 
@@ -75,11 +75,40 @@ Nodo * crearListaVacia(){
     return NULL;
 }
 
-Nodo * crearNodo(int id, int duracion, char Descripcion){
-    Nodo * nodo = (Nodo *)malloc(sizeof(Nodo));
-    nodo->T.TareaID = id;
-    nodo->T.Duracion = duracion;
-    nodo->T.Descripcion = Descripcion;
+Tarea cargarTarea(int id){
+    Tarea nuevaTarea;
+
+    char buffer[100];
+   
+    printf("\n---------Cargar tarea--------");
+    printf("\n\ningrese 'descripcion' de tarea pendiente: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")]= '\0';
+    nuevaTarea.Descripcion = (char *) malloc((strlen(buffer)+1) * sizeof(char));
+
+    if(nuevaTarea.Descripcion != NULL) {
+        strcpy(nuevaTarea.Descripcion, buffer);
+    }
+    nuevaTarea.Duracion = 10 + rand() % 91;
+
+    nuevaTarea.TareaID = id;
+
+    return nuevaTarea;
+}
+
+Nodo * crearNodo(Tarea nuevaTarea){
+    Nodo * nodo = (Nodo *)malloc(sizeof(Nodo));//reservo memoria para un nodo
+    
+    nodo->T = nuevaTarea;//carga con los datos  de muevaTarea
+
+    nodo->Siguiente = NULL;
 
     return nodo;
+}
+
+void insertarNodo(Nodo ** Start, Nodo * nuevoNodo){
+    //el nuevoNodo apunta a lo que antes era el primer elemento de la lista
+    nuevoNodo->Siguiente = *Start;
+    //el puntero inicial ahora apunta al nuevoNodo
+    *Start = nuevoNodo;
 }
