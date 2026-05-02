18af4872ab7132da487b8e110eda3923700abbe9
Alfredo-1 <alfr3d0zurita@gmail.com>
2026-05-01 23:14:37 -0300

    creacion de interfaz para elegir tareas y moverlas a otra lista

--- ModuloToDo.c
+++ ModuloToDo.c
@@ -19,18 +19,21 @@ Nodo * crearListaVacia();
 Nodo * crearNodo(Tarea nuevaTarea);
 Tarea cargarTarea(int id);
 void insertarNodo(Nodo ** Start, Nodo * nuevoNodo);
-Nodo * buscarNodo(Nodo ** Start, Tarea tarea);
+Nodo * buscarNodo(Nodo ** Start, int ID);
 void mostrarLista(Nodo * Start);
+Nodo * quitarNodo(Nodo ** Start, int ID);
+Nodo * eliminarNodo(Nodo * nodo);
 
 int main()
 {
     srand(time(NULL));
-    Nodo * start = crearListaVacia();
+    Nodo * start = crearListaVacia();//sera la lista de tareas a realizar
+    Nodo * start2 = crearListaVacia();//sera la lista de tareas realizadas
     Tarea tareaNueva;
     
     int condicion = 0;
     int opcion = 0;
-    static int id = 999;
+    int id = 999;
 
     do
     {
@@ -45,16 +48,45 @@ int main()
             printf("\nSu opcion: ");
             scanf("%d",&opcion);
             limpiarBuffer();
-        } while (opcion !=1 && opcion!=2);
+        } while (opcion !=1 && opcion!=2);//funcionara hasta que se ingrese una de las 2 opciones
         
-        if(opcion == 1){
+        if(opcion == 1)//cuando la opcion 1 es ingresada, condicion toma el valor 1
             condicion = 1;
+        else
+            condicion = 0;
+       
+    } while (condicion == 1);//va a iterar hasta que condicion = 1
+
+    printf("\n\n---------------Las tareas a realizar son------------");
+    mostrarLista(start);
+
+    
+    printf("\n\n-----Ingrese el ID de la tarea que desea mover a la lista de 'Tareas Realizadas'-----------\n");
+    do
+    {
+        printf("ID: ");
+        scanf("%i", &id);
+        limpiarBuffer();
+        Nodo * tareaBuscada = buscarNodo(&start, id);
+        if(tareaBuscada != NULL){
+            insertarNodo(&start2,quitarNodo(&start, id));//agregamos a la nueva lista el nodo que quitamos de la otra lista
         }else
+            printf("La tarea que esta buscando no existe");
+
+        printf("\n1. Mover otra tarea\n2. Finalizar\n");
+        do
+        {
+            printf("\nSu opcion: ");
+            scanf("%d",&opcion);
+            limpiarBuffer();
+        } while (opcion !=1 && opcion!=2);
+
+        if(opcion == 1)//cuando la opcion 1 es ingresada, condicion toma el valor 1
+            condicion = 1;
+        else
             condicion = 0;
 
-    } while (condicion == 1);
-
-    mostrarLista(start);
+    } while (condicion == 1);//dejara de iterar cuando condicion = 1
     
     return 0;
 }
@@ -109,13 +141,12 @@ void insertarNodo(Nodo ** Start, Nodo * nuevoNodo){
 
 void mostrarLista(Nodo * Start){//no hace falta pasarle con doble puntero por que no se cambiara ningun dato
     Nodo * Aux = Start;
-    printf("\n\n---------------Las tareas son------------");
-    while (Aux)//muestra hasta llegar a NULL
+     while (Aux)//muestra hasta llegar a NULL
     {
-        printf("\n\ndescripcion de tarea: %s", Aux->T.Descripcion);
-        printf("\nduracion de tarea: %d horas", Aux->T.Duracion);
-        printf("\nID Tarea: %d", Aux->T.TareaID);
-
+        printf("\n\nID Tarea: %d", Aux->T.TareaID);
+        printf("\nDescripcion de tarea: %s", Aux->T.Descripcion);
+        printf("\nDuracion de tarea: %d horas", Aux->T.Duracion);
+        
         Aux = Aux->Siguiente;
     }
 }
@@ -124,14 +155,36 @@ void mostrarLista(Nodo * Start){//no hace falta pasarle con doble puntero por qu
 de la tarea que quiero transferir a la lista de tareas finalizadas
 cambiar Tarea por id que recibe como parametro
 usar ese id para comparar con cada nodo de la lista hasta encontrarlo
-si no lo encuentra tengo que ver que retornar*/
-Nodo * buscarNodo(Nodo ** Start, Tarea tarea){
+si no lo encuentra retorna Aux = NULL*/
+Nodo * buscarNodo(Nodo ** Start, int ID){
     Nodo * Aux = * Start;
     //recorrera la lista hasta que aux llegue a null, o hasta que encuentre la tarea con el id buscado
-    while (Aux && Aux->T.TareaID != tarea.TareaID)
+    while (Aux && Aux->T.TareaID != ID)
     {
         //aux ira hasta el siguiente elemento del auxiliar
         Aux = Aux->Siguiente;
     }
     return Aux;
+}
+
+Nodo * quitarNodo(Nodo ** Start, int ID){
+    Nodo ** aux = Start;//apuntamos al puntero actual
+    //vaa recorrer la lista hasta encontrar el puntero buscado
+    while(*aux && (*aux)->T.TareaID != ID){
+        aux = &(*aux)->Siguiente;
+    }
+    //si encontramos el nodo con el id buscado, lo eliminamos de la lista y lo retornamos
+    if (*aux)
+    {
+        Nodo *temp = *aux;//guardamos el nodo que vamos a quitar en una variable temporal
+        *aux = (*aux)->Siguiente;//desvinculamos el nodo buscado de la lista
+        temp->Siguiente = NULL;//ponemos en NULL para asegurar no llevar vinculos fuera de la lista
+        
+        return temp;//retornamos el temp
+    }
+    return NULL;//si no se encontro el nodo buscado retornamos null.
+}
+
+Nodo * eliminarNodo(Nodo * nodo){
+    free(nodo);
 }
